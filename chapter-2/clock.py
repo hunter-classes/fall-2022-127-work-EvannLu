fa0d13eb86f065662647ed987c1d84fcc219c4ee
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-01 01:52:33 +0000

    added details in chp 2 > clock.py

---
+++ chapter-2/clock.py
@@ -0,0 +1,34 @@
+#Activity: 2.3.1 ActiveCode (ch02_20)
+
+print(3.14, int(3.14))
+print(3.9999, int(3.9999))       # This doesn't round to the closest int!
+print(3.0, int(3.0))
+print(-3.999, int(-3.999))        # Note that the result is closer to zero
+
+print("2345", int("2345"))        # parse a string to produce an int
+print(17, int(17))                # int even works on integers
+print(int("23bottles"))
+
+
+#Activity: 2.3.2 ActiveCode (ch02_21)
+
+print(float("123.45"))
+print(type(float("123.45")))
+
+
+
+#Activity: 2.3.3 ActiveCode (ch02_22)
+
+print(str(17))
+print(str(123.45))
+print(type(str(123.45)))
+
+
+
+#Activity: 2.3.4 Multiple Choice (test_question2_2_1)
+
+What value is printed when the following statement executes?
+
+print( int(53.785) )
+
+Answer: 53
