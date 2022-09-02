6918eb5b681e2ab4c6492a7b7e9dcbc098f6cd04
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-02 17:17:56 +0000

    Fixed hw

--- chapter-2/clock.py
+++ chapter-2/clock.py
@@ -1,34 +1,24 @@
-#Activity: 2.3.1 ActiveCode (ch02_20)
+#str var to ask question
+cTime_str = input("What time is it now (in hours)? ")
+wTime_str = input("How long (in hours) do you want your alarm to go off? ")
 
-print(3.14, int(3.14))
-print(3.9999, int(3.9999))       # This doesn't round to the closest int!
-print(3.0, int(3.0))
-print(-3.999, int(-3.999))        # Note that the result is closer to zero
+#int var to store user input 
+cTime_int = int(cTime_str)
+wTime_int = int(wTime_str)
 
-print("2345", int("2345"))        # parse a string to produce an int
-print(17, int(17))                # int even works on integers
-print(int("23bottles"))
+#get the time (in  24 hour)
+hour = cTime_int + wTime_int
 
+#turn the 24 hours + in 24 hour scale
+alarm = hour % 24
 
-#Activity: 2.3.2 ActiveCode (ch02_21)
+#print the desired time
+print(alarm)
 
-print(float("123.45"))
-print(type(float("123.45")))
-
-
-
-#Activity: 2.3.3 ActiveCode (ch02_22)
-
-print(str(17))
-print(str(123.45))
-print(type(str(123.45)))
+#check work
+#input = 0
+#alarm off 60 hours
+print(60 % 24)
+#works
 
 
-
-#Activity: 2.3.4 Multiple Choice (test_question2_2_1)
-
-What value is printed when the following statement executes?
-
-print( int(53.785) )
-
-Answer: 53
---
+++ chapter-4/turtles.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.screen()
+crush = turtle.Turtle()
+
+crush.forward(100)
+crush.right(90)
+crush.forward(50)
+
+wn.exitonclick()
+wn.mainloop()
+
+
