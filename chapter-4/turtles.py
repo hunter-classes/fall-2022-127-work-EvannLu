daeb3e8ad7671973afff008f211fa2e19d462fab
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-03 21:57:37 +0000

    added equilateral triangle movement

--- chapter-4/turtles.py
+++ chapter-4/turtles.py
@@ -1,12 +1,17 @@
-# set up alex
+#set up alex
 import turtle           
 wn = turtle.Screen()
 alex = turtle.Turtle()
 
-# repeat four times
+for i in [0, 1, 2]:
+  alex.forward(80)
+  alex.left(120)
+
+#repeat four times
 for i in [0, 1, 2, 3]:      
     alex.forward(50)
     alex.left(90)
+
 
 wn.exitonclick()
 
