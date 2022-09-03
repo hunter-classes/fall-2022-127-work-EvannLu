9c271dfcb184bccfe08288fb17531c38f11195c8
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-03 21:47:19 +0000

    edit "square" movement for turtle

--- chapter-4/turtles.py
+++ chapter-4/turtles.py
@@ -1,24 +1,21 @@
-import turtle
-
+# set up alex
+import turtle           
 wn = turtle.Screen()
+alex = turtle.Turtle()
 
-crush = turtle.Turtle()
+# repeat four times
+for i in [0, 1, 2, 3]:      
+    alex.forward(50)
+    alex.left(90)
 
-for i in [0, 1, 2, 3]:
-  crush.forward(90)
-  crush.right(90)
+wn.exitonclick()
 
-  
 #create second turtle
-
 squirt = turtle.Turtle()
 
 for i in [0, 1, 2]:
-  squirt.forward(90)
-  squrt.right(45)
-
+    squirt.forward(90)
+    squirt.right(45)
 
 wn.exitonclick()
 wn.mainloop()
-
-
