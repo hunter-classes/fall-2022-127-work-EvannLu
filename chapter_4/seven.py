ead05c20d0e4f6b7382303a9e6df4ba0560d3c11
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-04 03:44:11 +0000

    drunk pirate program revise

--- chapter_4/seven.py
+++ chapter_4/seven.py
@@ -9,6 +9,7 @@ for angle in [160, -43, 270, -97, -43, 200, -940, 17, -86]:
     drunk.left(angle)
     drunk.forward(100)
 
+#drunk.heading() gives the degree the drunk was facing at the end of the program
 print("The pirate's final heading was", drunk.heading())
 
 wn.exitonclick()
