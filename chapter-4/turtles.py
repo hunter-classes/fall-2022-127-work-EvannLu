bd13608640f8a214e6ed0923eb994e8531c2d2f7
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-04 02:52:21 +0000

    created 4th turtle for reference

--- chapter-4/turtles.py
+++ chapter-4/turtles.py
@@ -29,4 +29,17 @@ for i in [1,2]:
   rob.forward(150)
   rob.right(90)
 
-wn.exitonclick()
+
+
+#create fourth turtle
+jose = turtle.Turtle()
+jose.shape("turtle")
+jose.penup()
+
+for size in range(10):
+  jose.forward(50)
+  jose.stamp()
+  jose.forward(-50)
+  jose.right(36)
+
+wn.exitonclick
