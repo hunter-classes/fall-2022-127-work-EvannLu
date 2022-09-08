8a85311eb16f7e6c04f9c2d6253bc346b3a60491
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-08 13:33:44 +0000

    3rd edit

--- chapter-6/def.py
+++ chapter-6/def.py
@@ -31,11 +31,42 @@ square(squirt)
 wn.exitonclick()
 wn.mainloop()
 
+#Third
+
+def square(t,x,y,w,color,sidelen):
+
 #documentation screen is useful for organization. 
 #comment requires you to added new "#" every time you go to a new line
 #doc screen doesn't. 
+  
+    """
+    t - turtle
+    x,y - location
+    w - width
+    color - color to draw in
+    sidelen - length of each side
+    return:
+        nothing    
+    """
+    t.penup()
+    t.goto(x,y)
+    t.width(w)
+    t.color(color)
+    t.pendown()
 
-"""
+    for i in range(4):
+        t.forward(50)
+        t.right(90)
+wn = turtle.Screen()
+crush = turtle.Turtle()
+square(crush,100,100,5,"blue",4)
+squirt = turtle.Turtle()
+squirt.penup()
+squirt.goto(100,100)
+squirt.pendown()
+squirt.color("red")
+squirt.width(5)
+square(squirt)
 
-
-"""
+wn.exitonclick()
+wn.mainloop()
