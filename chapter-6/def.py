40e746706d2b42c3efc6638d3e74e3324772518f
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-08 13:10:09 +0000

    edits

--- chapter-6/def.py
+++ chapter-6/def.py
@@ -2,8 +2,23 @@ import turtle
 wn = turtle.Screen()
 crush = turtle.Turtle
 
-def square():
+#define function "square". However, not used. 
+def square(t):
   #draws a square
   for i in range(4):
-    foward(50)
-    right(90)
+    t.foward(50)
+    t.right(90)
+
+#use function
+square(crush)
+
+squirt = turtle.Turtle()
+squirt.penup()
+squirt.goto(100,100)
+squirt.pendown
+squirt.color("red")
+squirt.width(5)
+square(squirt)
+
+wn.exitonclick()
+wn.mainloop()
