5815d4cb12f0fdcdc03e0d7629366246482db73d
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-08 13:31:39 +0000

    edit 2nd

--- chapter-6/def.py
+++ chapter-6/def.py
@@ -1,24 +1,41 @@
 import turtle
-wn = turtle.Screen()
-crush = turtle.Turtle
 
-#define function "square". However, not used. 
+
+#define function "square". However, not used.
 def square(t):
-  #draws a square
+    #draws a square
   for i in range(4):
-    t.foward(50)
-    t.right(90)
+        t.forward(50)
+        t.right(90)
+
+
+wn = turtle.Screen()
+crush = turtle.Turtle()
 
 #use function
 square(crush)
 
+#second turtle
 squirt = turtle.Turtle()
+
+#move turtle to a new place
 squirt.penup()
-squirt.goto(100,100)
-squirt.pendown
+squirt.goto(100, 100)
+squirt.pendown()
 squirt.color("red")
 squirt.width(5)
+squirt.right(45)
+
 square(squirt)
 
 wn.exitonclick()
 wn.mainloop()
+
+#documentation screen is useful for organization. 
+#comment requires you to added new "#" every time you go to a new line
+#doc screen doesn't. 
+
+"""
+
+
+"""
