8dc26a69b031ff5c1f449eb251129289884f42fe
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-11 23:58:07 +0000

    create folder "HW-9-12" create file "turtle-functions.py"

---
+++ HW-9-12/turtle-functions.py
@@ -0,0 +1,7 @@
+'''
+Add the following routines:
+hexagon() -- this should have the same parameters as square and triangle but should draw a hexagon.
+ngon(t,numsides,x,y,color,width,sidelen) - this will draw a regular ngon with numsides sides.
+'''
+
+
