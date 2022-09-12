435c5b882db99e9631e3ddd326c3266edf59d9e0
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-12 01:58:51 +0000

    hexagon code --> turtle-functions.py

--- HW-9-12/turtle-functions.py
+++ HW-9-12/turtle-functions.py
@@ -2,6 +2,30 @@
 Add the following routines:
 hexagon() -- this should have the same parameters as square and triangle but should draw a hexagon.
 ngon(t,numsides,x,y,color,width,sidelen) - this will draw a regular ngon with numsides sides.
+ngon(t,numsides,x,y,color,width,sidelen)
 '''
+
+import turtle
+
+wn = turtle.Screen()
+crush = turtle.Turtle()
+
+def hexagon(t,numsides,x,y,color,width,sidelen):
+
+  t.penup()
+  t.goto(x,y)
+  t.color(color)
+  t.width(width)
+  t.pendown()
+  
+  for i in range(6):
+    t.forward(sidelen)
+    t.right(60)
+    
+
+hexagon(crush,6,75,75,"blue",10,69)
+
+wn.exitonclick()
+wn.mainloop()
 
 
