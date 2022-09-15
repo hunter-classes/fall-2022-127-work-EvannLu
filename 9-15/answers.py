5a23d623946dafde7ee2b4f71ae478a1c03cfc9a
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-15 00:17:44 +0000

    chp 7 question 8

--- 9-15/answers.py
+++ 9-15/answers.py
@@ -12,3 +12,14 @@ print(is_even(10))
 print(is_even(7))
 print(is_even(0))
 
+#Q8
+def is_odd(n):
+    if(n % 2 == 1):
+        return True
+    else:
+        return False
+
+print(is_odd(10))
+print(is_odd(7))
+print(is_odd(0))
+      
