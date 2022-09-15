abb386de5e069aa0e2ac622a9b136579d8d5ade8
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-15 00:17:01 +0000

    chp 7 question 7

--- 9-15/answers.py
+++ 9-15/answers.py
@@ -1,12 +1,14 @@
-#question 7
-from test import testEqual
+from cmath import atan
+from socket import BTPROTO_RFCOMM
 
+#Q7
 def is_even(n):
-    if n % 2 == 0:
+    if(n % 2 == 0):
         return True
     else:
         return False
 
-testEqual(is_even(100), True)
-testEqual(is_even(37), False)
-testEqual(is_even(0), True)
+print(is_even(10))
+print(is_even(7))
+print(is_even(0))
+
