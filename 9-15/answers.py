453b88254e16793e07d23bdf319d3de3c24569be
EvannLu <111663193+EvannLu@users.noreply.github.com>
2022-09-15 00:18:38 +0000

    chp 7 question 10-11

--- 9-15/answers.py
+++ 9-15/answers.py
@@ -23,3 +23,16 @@ print(is_odd(10))
 print(is_odd(7))
 print(is_odd(0))
       
+#Q10-11
+def is_rightangled(a, b, c):
+    at= a**2
+    bt = b**2
+    ct = c**2
+    
+    if(at+bt==ct or bt+ct==at or ct+at==bt):
+        return True
+    else:
+        return False
+
+print(is_rightangled(5,12,13))
+print(is_rightangled(3,4,6))
