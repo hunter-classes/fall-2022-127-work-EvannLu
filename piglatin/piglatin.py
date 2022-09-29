9346efdcefdb2b04eacbfa4c655e58086739da69
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-09-29 08:34:52 -0400

    updated code in piglain

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -12,19 +12,18 @@ print(bondify("Evan Lu"))
 
 
 def piglatin(word):
-    length = len(word)
     first = word[0].lower()
+    length = len(word)
+    punc = word[length-1]
     if(first in 'aeiou'):
-        if(word[length - 1] in '!,.?'):
-            punc = word[length - 1]
-            word = word[0:length - 1] + 'yay' + punc
-            return word
-        else:
-            if word[length - 1] in '!,.?':
-                punc = word[length - 1]
-                nword = word[1:length - 1] + first + "ay" + punc
-                return nword
+      if(word[length-1] in '.,?!'):
+        word = word[0:length - 1] + 'yay' + punc
+        return word.capitalize()
+    else:
+      word = word[1:length-1] + word[0].lower() + 'ay' + punc
+      return word.capitalize()
 
 print(piglatin("Aids!"))
+print(piglatin("Pigs!"))
 
 
