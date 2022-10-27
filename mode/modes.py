42119d76eac61b756151ecfed289847747e8c45b
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-10-27 09:38:47 -0400

    mode classwork

--- mode/modes.py
+++ mode/modes.py
@@ -14,6 +14,49 @@ def freq(l,v):
 
 
 
+'''
+classwork: create mode
+
+Strategy:
+- assue the first value is the mode 
+- we can grab its frequency 
+
+for each remaining item in the dataset" 
+    count that items frequence and see if
+    it's the new mode so far
+
+'''
+
+def mode(l): 
+    modeSoFar = l[0]
+    freqSoFar = freq(l,modeSoFar)
+    for i in l: 
+        if freq(l,i) > freqSoFar:
+            modeSoFar = i 
+            freqSoFar = freq (l,i)
+    return modeSoFar
+
+
+
+
 list = [23, 53, 65, 17, 9, 250, 100, 23]
 print(findLargest(list))
 print(freq(list, 23))
+print(mode(list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
