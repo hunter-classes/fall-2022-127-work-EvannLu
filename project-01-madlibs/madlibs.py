d9ec5c27b09154a362be9619780ee1d649753735
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-10-27 20:41:45 -0400

    delete

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -7,34 +7,3 @@ adjectives = ['attractive', 'dazzling', 'elegant', 'shapely', 'gorgeous', 'apath
 adverbs = ['abnormally', 'absentmindedly', 'accidentally', 'adventurously', 'anxiously', 'arrogantly', 'awkwardly', 'bashfully']
 heroes = random.choice(heroes)
 
-def madlibs(file):
-    retStr = ""
-    t = 0
-
-    heroes = ['Napoleon', 'Shakespeare', 'Da Vinci', 'Htiler', 'Aristotle', 'Jesus', 'Lincoln']
-    nouns = ['cow', 'kitty', 'secretary', 'rock', 'lion', 'cave', 'zookeeper', 'fish', 'bear', 'zebra']
-    verbs = ['run', 'dance', 'slide', 'jump', 'think', 'stand', 'smile', 'listen', 'eat', 'kill', 'run', 'punch', 'duck', 'scream']
-    adjectives = ['attractive', 'dazzling', 'elegant', 'shapely', 'gorgeous', 'apathetic', 'resolute', 'honorable', 'unnatural', 'young', 'severe']
-    adverbs = ['abnormally', 'absentmindedly', 'accidentally', 'adventurously', 'anxiously', 'arrogantly', 'awkwardly', 'bashfully']
-    heroes = random.choice(heroes)
-
-
-    f = open(file, "r")
-    str = f.read()
-    while (t < len(str)):
-        if (str[t:t + 6] == "{verb}"):
-            retStr += verbs[random.randint(0,len(verbs)-1)]
-            t+=6
-        elif (str[t:t + 6] == "{hero}"):
-            retStr += heroes
-            t+=6
-        elif (str[t:t + 6] == "{noun}"):
-            retStr += nouns[random.randint(0, len(nouns)-1)]
-            t+=6
-        else:
-            retStr += str[t]
-            t+=1
-    return retStr
-
-print("File: "  + open("story.txt","r").read() + "\n")
-print("After madlibs: " + madlibs("story.txt"))
