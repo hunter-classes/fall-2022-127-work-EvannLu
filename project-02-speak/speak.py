1b1b9961ee40c8302a92fa2f985a6bae13d8f9f9
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-11-27 15:39:06 -0500

    set up files

---
+++ project-02-speak/speak.py
@@ -0,0 +1,8 @@
+'''
+Extras: 
+1. Handle upper and lower case and/or punctuation. 
+2. Have an option to translate different languages. 
+3. Advanced translations like converting parts of words 
+rather than straight substitutions or inserting pirate phrases.
+'''
+
