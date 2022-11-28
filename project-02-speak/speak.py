9f41f47be118751eb9df3ca1c4b4617b0d1d8f14
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-11-27 22:29:48 -0500

    finish project

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,8 +1,8 @@
 '''
 Extras: 
 1. Handle upper and lower case and/or punctuation. 
-2. Have an option to translate different languages. 
-3. Advanced translations like converting parts of words 
+2. Another language translation. 
+2. Advanced translations like converting parts of words 
 rather than straight substitutions or inserting pirate phrases.
 '''
 
@@ -11,37 +11,48 @@ p = open('pirate.dat')
 rP = p.read()
 sP = rP.split('\n')
 
+r = open('randlang.dat')
+rR = r.read()
+sR = rR.split('\n')
+
 l = open('input.txt')
 rl = l.read().lower()
 sl = rl.split()
 
 
-# dict
-
-dict = {}
+# two dictionaries
+dictF = {}
 for word in sP:
     ind = word.find(':')
-    dict.update({word[0:ind]:word[ind+1:]})
+    dictF.update({word[0:ind]:word[ind+1:]})
+
+dictS = {}
+for word in sR: 
+    ind = word.find(':')
+    dictS.update({word[0:ind]:word[ind+1:]})
 
 # program
-nStory = []
-i = 0 
-for word in sl: 
-    if word in dict.keys(): 
-        nStory.append(dict[word])
-    else:
-        nStory.append(word)
-    if nStory[i] == nStory[0]:
-        nStory[i] = nStory[i].capitalize()
-    elif "." in nStory[i - 1]:
-        nStory[i] = nStory[i].capitalize()
-    elif "?" in nStory[i - 1]:
-        nStory[i] = nStory[i].capitalize()
-    elif "!" in nStory[i - 1]:
-        nStory[i] = nStory[i].capitalize()
-    i = i + 1
+def dictionary(dict): 
+    nStory = []
+    i = 0 
+    for word in sl: 
+        if word in dict.keys(): 
+            nStory.append(dict[word])
+        else:
+            nStory.append(word)
+        if nStory[i] == nStory[0]:
+            nStory[i] = nStory[i].capitalize()
+        elif "." in nStory[i - 1]:
+            nStory[i] = nStory[i].capitalize()
+        elif "?" in nStory[i - 1]:
+            nStory[i] = nStory[i].capitalize()
+        elif "!" in nStory[i - 1]:
+            nStory[i] = nStory[i].capitalize()
+    i = i + 1    
+    return " ".join(nStory)
 
-print(" ".join(nStory))
+print("First Translation:" + '\n' + dictionary(dictF) + '\n')
+print("Second Translation:" + '\n' + dictionary(dictS))
 
 
 
