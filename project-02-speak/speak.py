f6c8b2464b32fc343435ce0c398cf866c6003f2a
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-11-27 20:48:12 -0500

    finish project

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -6,3 +6,44 @@ Extras:
 rather than straight substitutions or inserting pirate phrases.
 '''
 
+# read files 
+p = open('pirate.dat')
+rP = p.read()
+sP = rP.split('\n')
+
+l = open('input.txt')
+rl = l.read().lower()
+sl = rl.split()
+
+
+# dict
+
+dict = {}
+for word in sP:
+    ind = word.find(':')
+    dict.update({word[0:ind]:word[ind+1:]})
+
+# program
+nStory = []
+i = 0 
+for word in sl: 
+    if word in dict.keys(): 
+        nStory.append(dict[word])
+    else:
+        nStory.append(word)
+    if nStory[i] == nStory[0]:
+        nStory[i] = nStory[i].capitalize()
+    elif "." in nStory[i - 1]:
+        nStory[i] = nStory[i].capitalize()
+    elif "?" in nStory[i - 1]:
+        nStory[i] = nStory[i].capitalize()
+    elif "!" in nStory[i - 1]:
+        nStory[i] = nStory[i].capitalize()
+    i = i + 1
+
+print(" ".join(nStory))
+
+
+
+
+
