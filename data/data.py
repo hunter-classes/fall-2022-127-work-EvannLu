6acad7f2982ff5f3001dacf1ef011d31d64fec7f
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-12-08 10:50:43 -0500

    prep

---
+++ data/data.py
@@ -0,0 +1,10 @@
+'''
+Instructions: 
+- Find one or more interesting data sources (examples in repo).
+- Write python program that does interesting analysis on one or more dataset. 
+- Should use Python csv module to read in data. 
+Extras: 
+- use matplotlib or another python graphing/plotting library. 
+- use mulitple aspects of a single data source in your analysis.
+- use more than one data source and have your analysis (3 C's).
+'''
