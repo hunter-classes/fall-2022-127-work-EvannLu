96fb10dee45c09553a4f008f3418141cb08032a7
Evan Lu <evan.l60@myhunter.cuny.edu>
2022-12-14 14:17:59 -0500

    finish data.py project

--- data/data.py
+++ data/data.py
@@ -1,10 +1,42 @@
-'''
-Instructions: 
-- Find one or more interesting data sources (examples in repo).
-- Write python program that does interesting analysis on one or more dataset. 
-- Should use Python csv module to read in data. 
-Extras: 
-- use matplotlib or another python graphing/plotting library. 
-- use mulitple aspects of a single data source in your analysis.
-- use more than one data source and have your analysis (3 C's).
-'''
+import csv 
+
+
+#prep
+f = open("cereal.csv", 'r') 
+reader = csv.reader(f)
+header = next(reader)
+header = next(reader)
+
+#dict
+cBrand = {}
+healthy_cBrand = []
+rating = 0
+x = ""
+
+for row in reader: 
+    #get cereal name, sodium, sugar   
+    cBrand = {'cereal':row[0], 'sodium': row[6], 'sugar': row[9]}
+    #if cereal has 0 sodium AND 0 sugar, get cereal in healthly_cBrand List
+    if(cBrand['sodium'] == '0' and cBrand['sugar'] == '0'):
+        healthy_cBrand.append(cBrand['cereal']) 
+
+    #Extra: compare ratings and get the best one
+    rat = float(row[15])
+    if (rat > rating): 
+        rating = rat
+        x = row[0]
+        
+print("These are some healthly choices of cereal: ", healthy_cBrand) #base project 
+print(x, "cereal has the highest rating of", rating) #Extra 
+
+
+
+
+
+    
+    
+   
+        
+
+
+
